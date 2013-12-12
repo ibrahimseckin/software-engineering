8a196201ad05e3a77fcd733ce3a6735e6b552d08
ibrahimseckin <ibrahim@ibrahim>
2013-12-12 19:04:22 +0200

    Config dosyasını oluşturan writeproperties classı.DaoConnect classı connect methodunda bir kere çalıştırılsa yeterli

---
+++ src/java/getServices/dao/WriteProperties.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package getServices.dao;
+
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ *
+ * @author TTISECKIN
+ */
+public class WriteProperties {
+
+    Properties prop = new Properties();
+
+
+
+    public WriteProperties() throws Exception {
+        try {
+            //set the properties value
+            prop.setProperty("serverName", "173.208.136.194");
+            prop.setProperty("sid", "test");
+            prop.setProperty("port", "3306");
+            prop.setProperty("databaseUser", "webapp");
+            prop.setProperty("databasePassword", "webapp14");
+
+            //save properties to project root folder
+            prop.store(new FileOutputStream("config.properties"), null);
+            
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
