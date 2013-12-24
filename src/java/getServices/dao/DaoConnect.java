0f6379318c7399b00f59a8af6b591496fd859984
ibrahimseckin <ibrahim@ibrahim>
2013-12-24 04:37:36 +0200

    database türkçe karakter sorunu çözüldü.

--- src/java/getServices/dao/DaoConnect.java
+++ src/java/getServices/dao/DaoConnect.java
@@ -17,12 +17,12 @@ public class DaoConnect {
     public  Statement connect() throws Exception {
         if(conn == null){
         Class.forName("com.mysql.jdbc.Driver");
-        conn = DriverManager.getConnection("jdbc:mysql://173.208.136.194:3306/test?useUnicode=true&characterEncoding=UTF-8", "webapp", "webapp14");
+        conn = DriverManager.getConnection("jdbc:mysql://173.208.136.194:3306/test?useUnicode=true&amp;characterEncoding=utf8", "webapp", "webapp14");
         Statement st1 = conn.createStatement();
         
         //st1.executeQuery("SET NAMES 'utf8'"); 
-        st1.executeQuery("SET CHARACTER SET utf8mb4"); 
-        st1.executeQuery("SET COLLATION_CONNECTION = 'utf8mb4_turkish_ci'"); 
+        //st1.executeQuery("SET CHARACTER SET utf8mb4"); 
+        //st1.executeQuery("SET COLLATION_CONNECTION = 'utf8mb4_turkish_ci'"); 
         
         return conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
         }
